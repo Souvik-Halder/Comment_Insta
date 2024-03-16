68a2daef59009d71fcb51b46d5ba9905d13094e1
Souvik-Halder <souvik.halder2020@gift.edu.in>
2024-03-17 02:25:37 +0530

    security update

--- src/pages/CommentPage.jsx
+++ src/pages/CommentPage.jsx
@@ -7,45 +7,46 @@ const CommentPage = () => {
   if (localStorage.getItem('passcode') !== '!amar_nam_anamika_das!') {
     window.location.href = '/';
     return;
-  }
-  useEffect(() => {
-    if (array.length <= 50) {
-      setTimeout(() => {
-        setArray((prev) => {
-          return [...prev, 'Bachha'];
-        });
-      }, 1000);
-    }
-    return () => {
-      clearTimeout();
-    };
-  }, [array]);
-  useEffect(() => {
-    console.log(array.length);
-    if (array.length >= 50) {
-      setShowText(true);
-    }
-  }, [array]);
-  return showText === false ? (
-    <div className='flex flex-wrap w-full gap-2 justify-center items-center'>
-      {Array.from(array).map((ele, index) => {
-        return (
-          <div
-            key={index}
-            className='text-[#fff] flex flex-col   font-[Gugi] text-3xl'
-          >
-            {ele}
-          </div>
-        );
-      })}
-    </div>
-  ) : (
-    <div className='w-full flex justify-center items-center h-screen'>
-      <div className='font-[Gugi] text-2xl'>
-        I Love U Baby 🫠 <span className='text-red-600 text-[28px]'>❤</span>
+  } else {
+    useEffect(() => {
+      if (array.length <= 50) {
+        setTimeout(() => {
+          setArray((prev) => {
+            return [...prev, 'Bachha'];
+          });
+        }, 1000);
+      }
+      return () => {
+        clearTimeout();
+      };
+    }, [array]);
+    useEffect(() => {
+      console.log(array.length);
+      if (array.length >= 50) {
+        setShowText(true);
+      }
+    }, [array]);
+    return showText === false ? (
+      <div className='flex flex-wrap w-full gap-2 justify-center items-center'>
+        {Array.from(array).map((ele, index) => {
+          return (
+            <div
+              key={index}
+              className='text-[#fff] flex flex-col   font-[Gugi] text-3xl'
+            >
+              {ele}
+            </div>
+          );
+        })}
       </div>
-    </div>
-  );
+    ) : (
+      <div className='w-full flex justify-center items-center h-screen'>
+        <div className='font-[Gugi] text-2xl'>
+          I Love U Baby 🫠 <span className='text-red-600 text-[28px]'>❤</span>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default CommentPage;
